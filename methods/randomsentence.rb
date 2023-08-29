f8e634ff56e1c64f731678d24d3dd4072de5dcf7
Kyle Barth <kb42misc@gmail.com>
2023-08-28 18:43:00 -0700

    methods exercises

---
+++ methods/randomsentence.rb
@@ -0,0 +1,16 @@
+def name(array)
+  array.sample
+end
+
+def activity(array)
+  array.sample
+end
+
+def sentence(n, a)
+  "#{n} went #{a} today!"
+end
+
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+puts sentence(name(names), activity(activities))
